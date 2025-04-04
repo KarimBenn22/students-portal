aeb78440e3b9e14eca832a9a261694bb1f575416
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2025-04-04 18:09:34 +0100

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,59 +1,48 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getCurrentSession } from "@/actions/actions";
+import { teacherAuthClient } from "./client/teacher-auth.client";
 
 function createRouteMatcher(paths: string[]) {
-    return (request: NextRequest): boolean => {
-        for (const path of paths) {
-            const escapedPath = path.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-            const regex = new RegExp(`^${escapedPath}$`);
-            if (regex.test(request.nextUrl.pathname)) {
-                return true;
-            }
-        }
-        return false;
-    };
+  return (request: NextRequest): boolean => {
+    for (const path of paths) {
+      const escapedPath = path.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      const regex = new RegExp(`^${escapedPath}$`);
+      if (regex.test(request.nextUrl.pathname)) {
+        return true;
+      }
+    }
+    return false;
+  };
 }
 
-const isGuestRoute = createRouteMatcher(["/signin", "/signup", "/"]);
-const isTeacherRoute = createRouteMatcher([]);
-const isStudentRoute = createRouteMatcher([]);
+const isGuestRoute = createRouteMatcher(["/signin", "/signup"]);
+const isTeacherRoute = createRouteMatcher(["/"]);
 
 export default async function middleware(request: NextRequest) {
-    /*const session = await getCurrentSession();
+  const { data: teacherSession } = await teacherAuthClient.getSession({
+    fetchOptions: {
+      headers: {
+        cookie: request.headers.get("cookie") ?? "",
+      },
+    },
+  });
 
-    // Handle authenticated users
-    if (session) {
-        const { role } = session.user;
+  // Handle authenticated Teachers
+  if (teacherSession) {
+    // Redirect authenticated teachers from guest routes
+    if (isGuestRoute(request)) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (isTeacherRoute(request)) {
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
+  }
 
-        // Redirect authenticated users from guest routes
-        if (isGuestRoute(request)) {
-            if (role === "teacher") {
-                return NextResponse.json({ error: "not implemented" }, { status: 500 });
-            } else if (role === "student") {
-                return NextResponse.json({ error: "not implemented" }, { status: 500 });
-            }
-        }
-
-        // Protect teacher routes
-        if (isTeacherRoute(request) && role !== "teacher") {
-            return NextResponse.json({ error: "not implemented" }, { status: 500 });
-        }
-
-        // Protect student routes
-        if (isStudentRoute(request) && role !== "student") {
-            return NextResponse.json({ error: "not implemented" }, { status: 500 });
-        }
-    } else {
-        if (isTeacherRoute(request) || isStudentRoute(request)) {
-            return NextResponse.redirect(new URL("/signin", request.url));
-        }
-    }*/
-
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        "/((?!api|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)"
-    ]
+  matcher: [
+    "/((?!api|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+  ],
 };
