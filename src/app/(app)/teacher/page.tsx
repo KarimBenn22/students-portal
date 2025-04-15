c46ab5506c1ab5ad958fe5958b5a4ad2e0584dc5
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-04-15 16:51:14 +0100

    update font

--- src/app/(app)/teacher/page.tsx
+++ src/app/(app)/teacher/page.tsx
@@ -1,5 +1,13 @@
+import { PageWrapper } from "@/components/layout/page-wrapper";
+
 export default function TeacherDashboardPage() {
-    return (
-        <div>Teacher overview</div>
-    );
+  return (
+    <PageWrapper>
+      <PageWrapper.Header
+        title="Overview"
+        description="Overview of your recent activity"
+      ></PageWrapper.Header>
+      <PageWrapper.Content></PageWrapper.Content>
+    </PageWrapper>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Geist } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/sonner"
 
-const inter = Inter({ subsets: ["latin"] })
+const geist = Geist({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Students Portal",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={geist.className}>
         {children}
         <Toaster />
       </body>
