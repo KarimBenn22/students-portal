8f348a925471782ba51ba470d62ebfccddadfdfe
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-04-19 10:31:42 +0100

    add logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
 export default function AuthLayout({
   children,
 }: {
@@ -12,7 +16,12 @@ export default function AuthLayout({
         <div className="absolute inset-0 bg-zinc-900 bg-[url('/uni-background.png')] bg-cover bg-center bg-no-repeat" />
       </div>
       <div className="lg:p-8 w-full">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 max-w-[500px]">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 max-w-[400px] items-center">
+          <Button asChild variant="ghost" className="w-fit mx-auto mb-4">
+            <Link href="/signin">
+              <Image src="/uni-logo.png" alt="University Logo" width={60} height={60} />
+            </Link>
+          </Button>
           {children}
         </div>
       </div>
