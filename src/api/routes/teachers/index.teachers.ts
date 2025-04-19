849ac02c207c602f3148cc518429dc7445fff34a
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2025-04-19 19:18:28 +0100

    link teacher proposals route

--- src/api/routes/teachers/index.teachers.ts
+++ src/api/routes/teachers/index.teachers.ts
@@ -1,9 +1,11 @@
 import factories from "@/api/factories";
 import roleMiddleware from "@/api/middleware/role.middleware";
 import teacherProjectsRoute from "./projects/index.projects";
+import teacherProposalsRoute from "./proposals/index.proposals";
 
 export default factories.teacher
   .createApp()
   .use(roleMiddleware("teacher"))
   .get("/", (c) => c.text("Teachers APIs"))
-  .route("/projects", teacherProjectsRoute);
+  .route("/projects", teacherProjectsRoute)
+  .route("/proposals", teacherProposalsRoute);
