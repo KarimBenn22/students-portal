8eac14e2f322c1773aee10d5660e9dd7cbdd4e49
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-04-20 08:36:26 +0100

    utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+dayjs.extend(relativeTime);
+
+export default function timeAgo(date: Date) {
+    return dayjs(date).fromNow();
 }
